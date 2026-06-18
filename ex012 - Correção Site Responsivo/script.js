5455260256442e344849a2c7723ff714e579190f
PedroMoura-23 <pedrohdemourasilva@gmail.com>
2026-06-17 21:49:57 -0300

    Atividades
    -Atividade Site Responsivo

---
+++ ex012 - Correção Site Responsivo/script.js
@@ -0,0 +1,9 @@
+function menu(){
+    let menu = document.getElementById('menu');
+    menu.classList.toggle('active')
+    
+}
+function cor(){
+    let html = document.documentElement;
+    nav.classList.toggle('escuro');
+}
